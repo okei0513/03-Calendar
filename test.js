06bb7ea86ff784575ebac9ec72317507470d40b4
okei0513 <keienne0513@icloud.com>
2021-05-19 22:20:33 +0900

    first

---
+++ test.js
@@ -0,0 +1,21 @@
+
+var table_box = document.getElementById("table_box");
+
+const list01 = ["日", "月", "火", "水", "木", "金", "土"]
+console.log(list01);
+
+const hyouji = [
+    { month: "1月", year: "2021年" },
+    { month: "2月", year: "2021年" },
+    { month: "3月", year: "2021年" },
+    { month: "4月", year: "2021年" },
+    { month: "5月", year: "2021年" },
+    { month: "6月", year: "2021年" },
+    { month: "7月", year: "2021年" },
+    { month: "8月", year: "2021年" },
+    { month: "9月", year: "2021年" },
+    { month: "10月", year: "2021年" },
+    { month: "11月", year: "2021年" },
+    { month: "12月", year: "2021年" },
+];
+
